2d80758be91d3c93ef3b8f340426b4168de533dc
Alexander Saye <alexandersaye@live.co.uk>
2013-02-04 16:47:13 +0000

    Updated Character Controller, Added Lightning
    Character can no longer jump.
    Character can now crouch.
    Character moves slower when crouching and moving backwards.
    Character emits low level eerie light.
    Lightning and thunder are on random timers.

---
+++ FebruaryGame/Assets/Scripts/Lightning.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using System.Collections;
+
+public class Lightning : MonoBehaviour
+{
+	public float FlashIntensity = 8.0f;
+	public float flashDelay = 0.0f;
+	private float flashTimer = 0.0f;
+	private float thunderDelay = 0.0f;
+	private float thunderTimer = 0.0f;
+	
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (light.intensity > 0.1f)
+		{
+			light.intensity *= 0.9f;
+		}
+		else
+		{
+			light.intensity = 0.0f;
+		}
+
+		// Basic timer for flash.
+		if (flashTimer >= flashDelay)
+		{
+			light.intensity = FlashIntensity;
+			
+			// Make a random delay until the next lightning strike to ensure a lightning strike WILL happen.
+			flashDelay = Random.Range(30, 120);
+			
+			// Give the thunder a random delay.
+			thunderDelay = Random.Range (1, 4);	
+
+			flashTimer = 0;
+		}
+		else
+		{
+			flashTimer += Time.deltaTime;
+		}
+		
+		if (thunderDelay != 0)
+		{
+			// Basic timer for thunder.
+			if (thunderTimer >= thunderDelay)
+			{
+				audio.volume = 1 - thunderDelay / 5;
+				audio.Play ();
+				
+				// Tell the timer to wait.
+				thunderDelay = 0;
+			}
+			else
+			{
+				thunderTimer += Time.deltaTime;
+			}
+		}
+	}
+}
---
+++ FebruaryGame/Assets/Scripts/NetworkManagerScript.js
@@ -0,0 +1,180 @@
+var playerPrefabEvil:GameObject;
+var playerPrefabGood:GameObject;
+var spawn1:GameObject;
+var spawn2:GameObject;
+var spawn3:GameObject;
+var spawn4:GameObject;
+var spawn5:GameObject;
+private var spawnObject:GameObject;
+var gameName:String = "OxyOxspringNetworking";
+private var stringToEdit : String = "Input Username";
+private var displayString : String = "";
+
+private var refreshing:boolean = false;
+private var hostData:HostData[];
+
+private var btnX:float;
+private var btnY:float;
+private var btnW:float;
+private var btnH:float;
+
+
+function Start(){
+	btnX = Screen.width * 0.05;
+	btnY = Screen.width * 0.05;
+	btnW = Screen.width * 0.1;
+	btnH = Screen.width * 0.1;
+	playerPrefabEvil.camera.enabled = true;
+	playerPrefabGood.camera.enabled = true;
+}
+
+function startServer(){
+Network.InitializeServer(32,25001,!Network.MovePublicAddress);
+MasterServer.RegisterHost(gameName,"Hunter Or Hunter", "Join Local Game");
+}
+
+function refreshHostList(){
+	MasterServer.RequestHostList(gameName);
+	refreshing = true;
+}
+
+function Update(){
+	if(refreshing == true){
+		if(MasterServer.PollHostList().Length > 0){
+		refreshing = false;
+		Debug.Log(MasterServer.PollHostList().Length);
+		hostData = MasterServer.PollHostList(); 
+		}
+	}
+}
+
+function spawnPlayer(){
+networkView.RPC("updateString",RPCMode.AllBuffered,stringToEdit);
+chooseSpawn();
+	Network.Instantiate(playerPrefabGood, spawnObject.transform.position, Quaternion.identity,0);
+	playerPrefabGood.camera.enabled = false;
+	playerPrefabEvil.camera.enabled = false;
+}
+
+function spawnHunter(){
+networkView.RPC("updateString",RPCMode.AllBuffered,stringToEdit);
+chooseSpawn();
+Network.Instantiate(playerPrefabEvil, spawnObject.transform.position, Quaternion.identity,0);
+	playerPrefabEvil.camera.enabled = false;
+	playerPrefabGood.camera.enabled = false;
+}
+
+function chooseSpawn(){
+var randomnumber = Random.Range(1,5);
+switch (randomnumber) {
+case 1:
+spawnObject = spawn1;
+break;
+case 2:
+spawnObject = spawn2;
+break;
+case 3:
+spawnObject = spawn3;
+break;
+case 4:
+spawnObject = spawn4;
+break;
+case 5:
+spawnObject = spawn5;
+break;
+}
+
+}
+
+//Messages
+function OnServerInitialized(){
+	Debug.Log("Server Initialised!");
+	//script.enabled = true;
+	spawnHunter();
+	
+}
+
+function OnConnectedToServer(){
+	spawnPlayer();
+	//script.enabled = true;
+}
+
+function OnDisconnectedFromServer(){
+	Application.LoadLevel(0);
+	//script.enabled = false;
+}
+
+function OnPlayerDisconnected(player: NetworkPlayer){
+	Debug.Log("Clean up after player " +  player);
+    Network.RemoveRPCs(player);
+    Network.DestroyPlayerObjects(player);
+}
+
+function OnMasterServerEvent(mse:MasterServerEvent){
+	if(mse == MasterServerEvent.RegistrationSucceeded){
+	Debug.Log("Registered Server");
+	}
+}
+
+//GUI
+function OnGUI(){
+	if(!Network.isClient && !Network.isServer){
+	stringToEdit = GUI.TextField(Rect(btnX,btnY,btnW,btnH/5),stringToEdit,16);
+		
+		if(GUI.Button(Rect(btnX,btnY * 1.5,btnW,btnH),"Start Server"))
+		{
+			Debug.Log("Starting Server");
+			startServer();
+		}
+		if(GUI.Button(Rect(btnX,btnY * 1.6 + btnH,btnW,btnH),"Refresh Hosts"))
+		{
+			Debug.Log("Refreshing");
+			refreshHostList();
+		}
+		
+		if(hostData){
+			for(var i:int = 0; i < hostData.length; i++){
+				if(GUI.Button(Rect(btnX * 2 + btnW, btnY * 1.2 + (btnH*i),btnW*3,btnH*0.5),hostData[i].comment)){
+					Network.Connect(hostData[i]);
+				}
+			}
+		}
+	}
+	else
+	{
+	GUI.Label(Rect(btnX-30,btnY-40,btnW*4,btnH/5),displayString);
+	}
+}
+
+function AttackedFish(){
+networkView.RPC("updateStringAttack",RPCMode.AllBuffered,stringToEdit);
+}
+
+function DeadFish(){
+networkView.RPC("updateStringDead",RPCMode.AllBuffered,stringToEdit);
+}
+
+function EscapeFish(){
+networkView.RPC("updateStringEscape",RPCMode.AllBuffered,stringToEdit);
+}
+
+@RPC
+function updateString(str:String){
+displayString = str + " has joined the game!";
+}
+
+
+@RPC
+function updateStringAttack(str:String){
+displayString = "The shark has bitten " + str + "!";
+}
+
+@RPC
+function updateStringDead(str:String){
+displayString = str + " has been devoured! Oh dear!";
+}
+
+@RPC
+function updateStringEscape(str:String){
+displayString = str + " has escaped from the shark...";
+}
