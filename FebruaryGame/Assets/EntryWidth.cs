b25b5c13e7abc8ee81e645f2e08e7eb09c68c40e
Alexander Saye <alexandersaye@live.co.uk>
2013-02-13 12:09:53 +0000

    General Commit

---
+++ FebruaryGame/Assets/EntryWidth.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class EntryWidth : MonoBehaviour
+{
+	public float W
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
