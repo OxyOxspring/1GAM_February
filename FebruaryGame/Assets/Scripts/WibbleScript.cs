b776edf5d43e3122150eb901a20240aa49af98ed
Alexander Saye <alexandersaye@live.co.uk>
2013-02-27 17:04:48 +0000

    Tentacles

---
+++ FebruaryGame/Assets/Scripts/WibbleScript.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+
+public class WibbleScript : MonoBehaviour
+{
+	float seed;
+	float scale;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		
+		seed = (transform.position.x * transform.position.y * transform.position.z * Random.Range(1,10000000000) * Time.deltaTime * transform.rotation.w) % Mathf.PI;
+		scale = transform.localScale.x;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (seed >= 2 * Mathf.PI)
+		{
+			seed = 0;
+		}
+		else
+		{
+			seed += Time.deltaTime;
+		}
+		
+		float seedything = Mathf.Sin(seed);
+		
+		transform.localScale = new Vector3(scale + seedything, scale + seedything, scale + seedything);
+		transform.Rotate (new Vector3(0,1,0), seedything / 10);
+	}
+}
